64013f36d71ac321fdb45e28d6e2ff134aefbde5
Philipp Muens <raute1337@gmx.de>
2019-11-04 14:04:18 +0100

    Add support for cron event

--- deploy/lib/getKnativeEventConfig.js
+++ deploy/lib/getKnativeEventConfig.js
@@ -1,13 +1,35 @@
 'use strict'
 
-const validEvents = ['custom', 'cronJob']
+const validEvents = ['custom', 'cron']
 const knativeVersion = 'v1alpha1'
 
+// TODO: update this when we're dealing with services other
+// than the ones deployed by the Serverless Framework (e.g. K8S services)
 function getRef(sinkName) {
   return {
     apiVersion: `serving.knative.dev/${knativeVersion}`,
     kind: 'Service',
     name: sinkName
+  }
+}
+
+function getCronConfig(sinkName, eventConfig) {
+  const { schedule, data } = eventConfig
+  if (!schedule) {
+    throw new Error('"schedule" configuration missing for cron event.')
+  }
+  if (!data) {
+    throw new Error('"data" configuration missing for cron event.')
+  }
+  return {
+    kind: 'CronJobSource',
+    knativeGroup: 'sources.eventing.knative.dev',
+    knativeVersion,
+    spec: {
+      schedule,
+      data,
+      sink: getRef(sinkName)
+    }
   }
 }
 
@@ -32,7 +54,9 @@ function getKnativeEventConfig(sinkName, eventName, eventConfig) {
     return false
   }
 
-  // TODO: `if (eventName === 'fooBar') { ... }`
+  if (eventName === 'cron') {
+    return getCronConfig(sinkName, eventConfig)
+  }
 
   return getCustomConfig(sinkName, eventConfig)
 }
